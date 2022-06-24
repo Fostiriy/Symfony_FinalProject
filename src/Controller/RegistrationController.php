3bec7444c2665e9589eb2ec36ab4807c4a69697f
Fostiriy <burakov.aa@students.dvfu.ru>
2022-06-24 19:29:55 +1000

    Внести итоговые правки

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -36,7 +36,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_toy_index');
+            return $this->redirectToRoute('app_main');
         }
 
         return $this->render('registration/register.html.twig', [
